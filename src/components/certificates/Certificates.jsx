fe6f5ee20f479c8e75fa59ef5c3a7597ce9c3a26
Kaleb <kaleb.coyo@ti.ukdw.ac.id>
2025-10-25 12:34:57 +0700

    add certificates

--- src/components/certificates/Certificates.jsx
+++ src/components/certificates/Certificates.jsx
@@ -2,13 +2,33 @@ import React from 'react'
 import './Certificates.css'
 import CertificateBlock from './CertificateBlock'
 
+import deeplearningcert from '../../assets/certificates/deeplearning.jpg'
+import machinelearningcert from '../../assets/certificates/machinelearning.jpg'
+import tensorflowcert from '../../assets/certificates/tensorflow.jpg'
+import bangkitcert from '../../assets/certificates/bangkit_front.jpg'
+
 const Certificates = () => {
   return (
-    <div>
+    <div className='certificate-section'>
         <CertificateBlock
-        folderpath={'../../assets/certificates/deeplearning.jpg'}
-        name={'DeepLearning.AI TensorFlow Developer Certificate'}
-        date={'November 11, 2023'}/>
+        folderpath={machinelearningcert}
+        name='Coursera Machine Learning Certificate'
+        date='October 22, 2023'/>
+
+        <CertificateBlock
+        folderpath={deeplearningcert}
+        name='DeepLearning.AI TensorFlow Developer Certificate'
+        date='November 11, 2023'/>
+
+        <CertificateBlock
+        folderpath={bangkitcert}
+        name='Bangkit Academy 2023 Certificate'
+        date='January 19, 2024'/>
+
+        <CertificateBlock
+        folderpath={tensorflowcert}
+        name='TensorFlow Developer Certificate'
+        date='April 17, 2024'/>
     </div>
   )
 }
--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -8,10 +8,10 @@ const Navbar = () => {
       {/* <img src={logo} alt=""/> */}
       <h1 className='namaJudul'>Kaleb Coyo Wagito</h1>
       <ul className='navmenu'>
-        <li>About Me</li>
-        <li>Certificates</li>
-        <li>Projects</li>
-        <li>Contact</li>
+        <li className='navbar-menu'>About Me</li>
+        <li className='navbar-menu'>Certificates</li>
+        <li className='navbar-menu'>Projects</li>
+        <li className='navbar-menu'>Contact</li>
       </ul>
       <div className="navconnect">Connect</div>
     </div>
