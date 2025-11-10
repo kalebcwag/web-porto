dc843e2a92389b734229a232bd7ce5d362425c8b
Kaleb <kaleb.coyo@ti.ukdw.ac.id>
2025-11-10 11:13:07 +0700

    Update About.jsx

--- src/components/about/About.jsx
+++ src/components/about/About.jsx
@@ -19,7 +19,7 @@ const About = () => {
                 <img src={foto} alt="" className='pic'/>
             </div>
             <div className="text-and-social">
-                <p>Highly enthusiastic <span>fresh graduate</span> with strong passion in <span className='highlight'>AI</span>, <span className="highlight">Machine Learning</span>, <span className="highlight">Data Science</span>, and <span className="highlight">Data Analysis</span>. My core skill is <span className="highlight">Python</span>, including libraries like <span className="highlight">Tensorflow</span>, <span className="highlight">Keras</span>, <span className="highlight">Sci-kit Learn</span>, <span className="highlight">Pandas</span>, <span className="highlight">NumPy</span>, <span className="highlight">Matplotlib</span> needed for machine learning model development and data processing.</p>
+                <p>Highly enthusiastic <span>fresh graduate</span> with strong passion in <span className='highlight'>AI</span>, <span className="highlight">Machine Learning</span>, and <span className="highlight">Data Science</span>. My core skill is <span className="highlight">Python</span>, including libraries like <span className="highlight">Tensorflow</span>, <span className="highlight">Keras</span>, <span className="highlight">Sci-kit Learn</span>, <span className="highlight">Pandas</span>, <span className="highlight">NumPy</span>, <span className="highlight">Matplotlib</span> needed for machine learning model development and data processing.</p>
                 <ul className='socials'>
                     <li><img src={linkin} alt="" /></li>
                     <li className='txt'><a href="https://linkedin.com/in/kalebcoyowagito" target='_blank' rel='noopener noreferrer'>kalebcoyowagito</a></li>
